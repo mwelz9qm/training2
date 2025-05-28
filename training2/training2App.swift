2cf305bd9d1457603bd757f170dc29467c2c3481
Matt Welz <matthew.d.welz@gmail.com>
2025-05-28 13:05:20 -0600

    Initial Commit

---
+++ training2/training2App.swift
@@ -0,0 +1,17 @@
+//
+//  training2App.swift
+//  training2
+//
+//  Created by admin on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct training2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
